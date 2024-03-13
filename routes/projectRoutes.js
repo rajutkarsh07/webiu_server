d0ad867d60de00dd9ff4d2d1e3b9bfe65afb906a
utkarsh raj <rajutkarsh2505@gmail.com>
2024-03-13 19:54:41 +0530

    fixed routes for projects

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -4,8 +4,8 @@ const projectController = require('./../controllers/projectController');
 const router = express.Router();
 
 router.get('/projects', projectController.getAllProjects);
-router.get('/:name', projectController.getRepositoryData);
-router.get('/allContributor', projectController.getAllContributors);
-router.get('/:name', projectController.getRepositoryContributors);
+router.get('/repository/:name', projectController.getRepositoryData);
+router.get('/contributors', projectController.getAllContributors);
+router.get('/contributors/:name', projectController.getRepositoryContributors);
 
 module.exports = router;
