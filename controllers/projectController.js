47d89fd5796dc26c3b7183f105ca1eaeca1a6cd3
utkarsh raj <rajutkarsh2505@gmail.com>
2024-03-13 07:02:20 +0530

    added userCreatedIssues and userCreatedPullRequests controller

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -84,3 +84,52 @@ exports.repositoryContributors = catchAsync(async (req, res, next) => {
     res.status(500).json({ err: "Internal server error" });
   }
 });
+
+exports.userCreatedIssues = catchAsync(async (req, res, next) => {
+    try {
+      const { username } = req.params;
+  
+      const issuesResponse = await axios.get(
+        `https://api.github.com/search/issues?q=author:${username}+org:c2si+type:issue`,
+        {
+          headers: {
+            Authorization: `token ${accessToken}`
+          }
+        }
+      );
+      const issues = issuesResponse.data.items;
+  
+      res.status(200).json({ issues });
+    } catch (error) {
+      console.error(
+        "Error fetching user created issues:",
+        error.response ? error.response.data : error.message
+      );
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+  
+  exports.userCreatedPullRequests = catchAsync(async (req, res, next) => {
+    try {
+      const { username } = req.params;
+  
+      const pullRequestsResponse = await axios.get(
+        `https://api.github.com/search/issues?q=author:${username}+org:c2si+type:pr`,
+        {
+          headers: {
+            Authorization: `token ${accessToken}`
+          }
+        }
+      );
+      const pullRequests = pullRequestsResponse.data.items;
+  
+      res.status(200).json({ pullRequests });
+    } catch (error) {
+      console.error(
+        "Error fetching user created pull requests:",
+        error.response ? error.response.data : error.message
+      );
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+  
